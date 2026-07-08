c04c324f03331aa12337cee414edbec7b370a81e
prince22466 <prince2246@sina.com.cn>
2026-07-08 12:13:30 +0200

    py scripts for checking and zipping

---
+++ Co_Kaggle/g3/submission_onnx/onnx_checker.py
@@ -0,0 +1,8 @@
+# this py script is to check each subfolder
+# has the onnx file it is supposed to have
+# aka, effective submission with score
+# and print out the missing onnx which has effective score
+# this script should check the file name info
+# in files like 05_fill_additive_marking_local_3x3.md in submission_records,
+# which tells what task submission is solved(aka, effective submission)
+# should report if there are duplicates, 
